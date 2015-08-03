66afab9d1d31c56cc29fb8e0c7261357fab2918e
Tyler Guillen <tyguillen@gmail.com>
2015-08-03 00:57:52 -0700

    js now working

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,9 @@
-$(document).ready(function(){
-
+$(document).ready(function() {
+	$('.job a').mouseenter(function() {
+		$(this).fadeTo('fast', .5);
+	});
+	
+	$('.job a').mouseleave(function() {
+		$(this).fadeTo('fast', 1);
+	});
 });
